65bcfe406161ea776f155ee822c811d03abe46f8
manjunath2708 <manju92ng@gmail.com>
2024-12-23 22:47:50 +0530

    added variables

---
+++ Basic/Day1-3.py
@@ -0,0 +1,30 @@
+# Variables (creating and using)
+# refer inputs in future uses 
+
+name = input("\n enter name")
+print(name)
+
+#len function to measure the char len take user input and calculate the lenght 
+
+
+print(len("name"))
+
+#all 3 commands can be used in same line
+
+print(len(input("enter Name ")))
+username = input("enter name pls")
+length = len(username)
+print(length)
+
+# swap variable
+glass1 = input("milk")
+glass2 = input("juice")
+
+# glass1, glass2 = glass2, glass1
+
+# glass1 = "milk"
+# glass2 = "juice"
+
+# glass1, glass2 = glass2, glass1
+
+# print("glass1, glass2")
---
+++ Basic/test.py
@@ -0,0 +1,13 @@
+glass1 = input("milk")
+glass2 = input("juice")
+
+# glass1, glass2 = glass2, glass1
+
+print("glass1")
+
+# glass1 = "milk"
+# glass2 = "juice"
+
+# glass1, glass2 = glass2, glass1
+
+# print("glass1, glass2")
