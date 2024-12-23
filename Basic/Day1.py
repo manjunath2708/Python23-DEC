9299b81a7ba8639be6a59562473176d7d61f9435
manjunath2708 <86849175+manjunath2708@users.noreply.github.com>
2024-12-23 21:08:30 +0530

    Create Day1.py

---
+++ Basic/Day1.py
@@ -0,0 +1,11 @@
+print("hello")
+
+#add\n for printing in next line
+
+print("hello\n test line3")
+
+#concattenation adding another sting 
+print("hello" + "test")
+#to add  space 
+print("hello" + " " + "tetst")
+
