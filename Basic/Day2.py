ce1a00d76fde42d8710efe6312acca67d6fce1eb
manjunath2708 <manju92ng@gmail.com>
2024-12-24 15:39:43 +0530

    day2

---
+++ Basic/Day2.py
@@ -0,0 +1,12 @@
+#data types ,numbers, operations,type conversions , f-strings
+#Data types in python >string>interger>floats>boolean
+
+#subscripting
+print("Hello"[-1])
+
+
+#string
+print("123" + "345")
+
+#integer = whole number
+print(123 + 345)
