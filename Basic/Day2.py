174b37436b13ad15054e261309cc0779cced34eb
manjunath2708 <manju92ng@gmail.com>
2024-12-25 16:24:20 +0530

    Day2 data types

--- Basic/Day2.py
+++ Basic/Day2.py
@@ -10,3 +10,25 @@ print("123" + "345")
 
 #integer = whole number
 print(123 + 345)
+
+
+#large integers
+print(12_3333_4444_4445)
+#float
+print(3.14)
+
+#Boolean
+print(False)
+print(True)
+
+
+#types
+print(type("abv"))
+print(type(123))
+print(type(3.14))
+print(type(True))
+
+name_of_the_user = input("Enter your Name")
+length_of_Name = len(name_of_the_user)
+print(type("Number of letters in yopur Name: "))
+print(type(length_of_Name))
