426aa2d70060ea29e16068f518993ee86224f517
manjunath2708 <manju92ng@gmail.com>
2024-12-25 16:31:05 +0530

    PEMDAS

---
+++ Basic/Day3.py
@@ -0,0 +1,12 @@
+#calculations
+
+print("my age: " + str(20))
+print(123 + 456)
+print(10 - 6)
+print(3 * 2)
+print(50 / 2)
+print(50 // 2)
+print(3*3)
+print(3**3)
+
+#** EXPO ,* IS MULTTIPLE <pemdas
